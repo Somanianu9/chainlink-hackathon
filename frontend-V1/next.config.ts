be8fa7d5a40ae9a7104d334f73556b590f56fe42
Somanianu9 <somanianushka9@gmail.com>
2025-07-03 03:42:15 +0530

    Update next.config.ts

--- frontend-V1/next.config.ts
+++ frontend-V1/next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-   typescript: {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
     ignoreBuildErrors: true
   },
   eslint: {
@@ -9,4 +8,4 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
